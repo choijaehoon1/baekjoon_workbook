339dcc0505aba02c14c1f59e224b35495fcf0945
최재훈 <wogns_20@naver.com>
2021-08-02 22:19:41 +0900

    test72.py commit

---
+++ src/test72.py
@@ -0,0 +1,29 @@
+import sys
+
+data = sys.stdin.readline().rstrip()
+stack = []
+
+answer = ''
+for i in data:
+    if i.isalpha():
+        answer += i
+    else:
+        if i == '(':
+            stack.append(i)
+        elif i == '*' or i == '/':
+            while stack and (stack[-1] == '*' or stack[-1] == '/'):
+                answer += stack.pop()
+            stack.append(i)
+        elif i == '+' or i == '-':
+            while stack and stack[-1] != '(':                        
+                answer += stack.pop()
+            stack.append(i)
+        elif i == ')':
+            while stack and stack[-1] != '(':                        
+                answer += stack.pop()
+            stack.pop()
+while stack:
+    answer += stack.pop()
+print(answer)    
+
+
