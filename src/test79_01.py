8dbfc6adc1d3c73bff5b42d3bd5977a21d334315
최재훈 <wogns_20@naver.com>
2021-08-16 23:00:02 +0900

    test79_01.py commit

---
+++ src/test79_01.py
@@ -0,0 +1,31 @@
+import sys
+from collections import Counter
+import decimal
+
+array = []
+
+cnt = 0
+while True:
+    tmp = sys.stdin.readline().rstrip()
+    if tmp == '':
+        break
+    array.append(tmp)
+    cnt += 1
+tmp_list = list(Counter(array).most_common())
+
+data = []
+
+num = 100/cnt
+
+for i in tmp_list:
+    s_num = str(i[1]*num)
+    bi = round(decimal.Decimal(s_num),4)
+    data.append([i[0],bi])
+result = sorted(data, key=lambda x:x[1],reverse=True)    
+answer = sorted(result, key=lambda x:x[0])    
+# print(answer)
+
+for i in answer:
+    print(i[0],i[1])
+
+
